02af9eeeb8c5de5fec90cdf7130e8a2bed36373c
g.v.muenchow <gvm@evm-net.de>
2016-03-17 11:41:23 +0100

    initial 2

---
+++ eCARus_SS15(VERSION01)/events_0x0.h
@@ -0,0 +1,14 @@
+#ifndef GTF_INCLUDED_EVENTS_IDENTIFIERS_0x0_H
+#define GTF_INCLUDED_EVENTS_IDENTIFIERS_0x0_H
+
+#include <GtfTypes/GtfStdDef.h>
+
+//! @brief ExtOut_Navigation_Select_City
+//!
+//! Parameters : 1
+//! 1) City_Index::int32_t
+#define EVENT_GRP_ID_ExtOut_Navigation_Select_City    ((uint32_t) 0x00000000)
+#define EVENT_MSG_ID_ExtOut_Navigation_Select_City    ((uint32_t) 0x00000000)
+
+#endif //GTF_INCLUDED_EVENTS_IDENTIFIERS_0x0_H
+
